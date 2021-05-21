cf89190cf7957385df3d32343627244dcce51c68
aunne <unihae236@gmail.com>
2021-05-22 02:19:24 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import message_text_async
 
+
 class MyClient(discord.Client):
     # test to echo
     async def on_message(self, message):
@@ -10,13 +11,12 @@ class MyClient(discord.Client):
     # print ready after bot turn on
     async def on_ready(self):
         await message_text_async.print_ready(self)
-        
 
 
-assert os.path.isfile("TOKEN")
-with open("TOKEN", "r") as file:
-    TOKEN = file.read().strip()
-
-client = MyClient()
 if __name__ == "__main__":
+    assert os.path.isfile("TOKEN")
+    with open("TOKEN", "r") as file:
+        TOKEN = file.read().strip()
+
+    client = MyClient()
     client.run(TOKEN)
